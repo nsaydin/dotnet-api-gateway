ca0a60232200fe92051ce1c71ac59f8f2215aa8f
Enes Aydın <enaydin@protel.com.tr>
2022-09-13 15:48:20 +0300

    Added Order Api

--- src/Identity.Api/Auth/Config.cs
+++ src/Identity.Api/Auth/Config.cs
@@ -21,9 +21,22 @@ namespace Identity.Service.Auth
                     AllowOfflineAccess = true,
                     RefreshTokenExpiration = TokenExpiration.Sliding,
                     RefreshTokenUsage = TokenUsage.OneTimeOnly,
-                    AllowedScopes = { "user.api.scope" },
+                    AllowedScopes = { "user.api.scope", "order.api.scope" },
                     UpdateAccessTokenClaimsOnRefresh = true,
-                    ClientSecrets = { new Secret("user.api.secret".Sha256()) },
+                    ClientSecrets = { new Secret("user.api.secret".Sha256()) }
+                },
+                new()
+                {
+                    ClientId = "order.api.client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AccessTokenLifetime = 36000,
+                    IdentityTokenLifetime = 36000,
+                    AllowOfflineAccess = true,
+                    RefreshTokenExpiration = TokenExpiration.Sliding,
+                    RefreshTokenUsage = TokenUsage.OneTimeOnly,
+                    AllowedScopes = { "order.api.scope" },
+                    UpdateAccessTokenClaimsOnRefresh = true,
+                    ClientSecrets = { new Secret("order.api.secret".Sha256()) }
                 }
             };
         }
@@ -33,6 +46,7 @@ namespace Identity.Service.Auth
             return new List<ApiResource>
             {
                 new("user.api.resource") { Scopes = { "user.api.scope" } },
+                new("order.api.resource") { Scopes = { "order.api.scope" } },
             };
         }
 
@@ -41,23 +55,42 @@ namespace Identity.Service.Auth
             return new List<ApiScope>
             {
                 new("user.api.scope"),
+                new("order.api.scope")
+                {
+                    UserClaims = new List<string>
+                    {
+                        "order-api-access"
+                    }
+                },
             };
         }
 
         public static List<TestUser> GetUsers()
         {
-            return new()
+            return new List<TestUser>
             {
-                new TestUser()
+                new()
                 {
                     Username = "superUser",
-                    Password = "superPassword",
+                    Password = "superUser",
                     IsActive = true,
                     SubjectId = Guid.Parse("a78f94cd-32e8-499f-8574-66073e1f8616").ToString(),
                     Claims = new List<Claim>
                     {
                         new("birthDate", DateTime.Now.ToShortDateString()),
-                        new("age", "30")
+                        new("age", "30"),
+                    }
+                },
+                new()
+                {
+                    Username = "userApiIntegration",
+                    Password = "userApiIntegration",
+                    IsActive = true,
+                    SubjectId = Guid.Parse("2f5ada4e-dc36-4e76-966b-edfac977e342").ToString(),
+                    Claims = new List<Claim>
+                    {
+                        new("birthDate", DateTime.Now.ToShortDateString()),
+                        new("age", "22")
                     }
                 }
             };
---
+++ src/Order.Api/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product.Service.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[]
+            {
+                "Order-1", "Order-2", "Order-3"
+            };
+        }
+    }
+}
